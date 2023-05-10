67e2313c198d5d516fa74730efc251c761af579c
Nagydani11 <nagydani97@gmail.com>
2023-05-10 14:36:33 +0200

    Login

---
+++ projekt/projekt12/src/components/Login/Login.js
@@ -0,0 +1,12 @@
+export default function Login(){
+    return(
+        <>
+            <h1>Log in</h1>
+            <h4>Email</h4>
+            <input type="text" />
+            <h4>Jelszó</h4>
+            <input type="text" />
+            <p><button>Belépés</button></p>
+        </>
+    )
+}
