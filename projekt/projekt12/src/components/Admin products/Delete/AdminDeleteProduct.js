cd4b814331a864752ab8df5aa7cb4e59305ed3db
Pinke2k <pinkemihaly@gmail.com>
2023-05-10 20:04:01 +0200

    add admin delete

--- projekt/projekt12/src/components/Admin products/Delete/AdminDeleteProduct.js
+++ projekt/projekt12/src/components/Admin products/Delete/AdminDeleteProduct.js
@@ -1,5 +1,32 @@
-const AdminDeleteProduct = () => {
-	return <div>delete</div>;
-};
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteProduct, readProducts } from "../../../Services/Crud";
+import formatData from "../../../Utils/formdata";
 
-export default AdminDeleteProduct;
+export default function AdminDeleteProduct() {
+	const [productDelete, setProductDelete] = useState("");
+	const { id } = useParams();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		readProducts().then((data) => {
+			formatData(data);
+			setProductDelete(data.title);
+		});
+	}, [id]);
+
+	const deleteProductfgv = () => {
+		deleteProduct(id).then((json) => navigate("/admin/termekek"));
+	};
+
+	return (
+		<div className="delete-product">
+			<button className="delete-btn" onClick={deleteProductfgv}>
+				Törlés
+			</button>
+			<button className="cancel-btn" onClick={() => navigate("/admin/termekek")}>
+				Mégse
+			</button>
+		</div>
+	);
+}
