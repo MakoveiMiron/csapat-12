a51dae753add7d1b970e31cacefa7e1291d9902a
Nagydani11 <nagydani97@gmail.com>
2023-05-11 16:59:22 +0200

    Login

--- projekt/projekt12/src/components/Login/Login.js
+++ projekt/projekt12/src/components/Login/Login.js
@@ -1,12 +1,27 @@
+import {useState} from 'react'
+
 export default function Login(){
+    const [login, setLogin] = useState("")
+
+    function loginHandler(e){
+        e.preventDefault()
+
+        const email = e.target.elements.email.value;
+        const password = e.target.elements.pw.value;
+        const obj={
+            email,
+            password
+        }
+
+        console.log(obj)
+    }
     return(
-        <>
-            <h1>Log in</h1>
-            <h4>Email</h4>
-            <input type="text" />
-            <h4>Jelszó</h4>
-            <input type="text" />
-            <p><button>Belépés</button></p>
-        </>
+        <form onSubmit={loginHandler}>
+            <h3>Email cim</h3>
+            <p><input type='email' name='email' placeholder='E-mail cím' /></p>
+            <h3>Jelszó</h3>
+            <p><input type='password' name='pw' placeholder='jelszó' /></p>
+            <button type='submit'>Belépés</button>
+        </form>
     )
 }
