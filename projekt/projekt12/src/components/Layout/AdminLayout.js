16d7d1aa303331c1a3d844cb7315631e6ea923c1
MakoveiMiron <miron.makovei@gmail.com>
2023-05-14 20:34:35 +0200

    csak az admin lassa az admin oldalakat kesz

--- projekt/projekt12/src/components/Layout/AdminLayout.js
+++ projekt/projekt12/src/components/Layout/AdminLayout.js
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
 import AdminNavigation from "../Nav/AdminNavigation";
-
+import { useContext } from "react";
+import { LoggedInUserContext } from "../../contexts/LoggedInUserContext";
 export default function AdminLayout(){
+
+    const [user,setUser] = useContext(LoggedInUserContext);
+
     return(
         <>
-            <AdminNavigation />
-            <Outlet/>
+        {user.uid === 'KqiGKLztHcVTxJukIfzbmUMgD8E3' ? <>
+                <AdminNavigation />
+                <Outlet/>
+                </> 
+                : 
+                "Ez csak az admin számára elérhető!"
+        }
         </>
     )
 }
