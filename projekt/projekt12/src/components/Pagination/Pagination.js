ed7c06e17a2469040fbaba8cd6b94b8ab14007f0
Pinke2k <pinkemihaly@gmail.com>
2023-05-15 19:34:37 +0200

    add advanced pagination

--- projekt/projekt12/src/components/Pagination/Pagination.js
+++ projekt/projekt12/src/components/Pagination/Pagination.js
@@ -1,13 +1,13 @@
 import "./pagination.css";
 import React, { useState, useEffect } from "react";
 import { FiChevronsLeft } from "react-icons/fi";
-import {FiChevronLeft} from "react-icons/fi"
-import {FiChevronRight} from "react-icons/fi"
-import {FiChevronsRight} from "react-icons/fi"
+import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronRight } from "react-icons/fi";
+import { FiChevronsRight } from "react-icons/fi";
 
 function Pagination({ limit, onPageChange, currentPage, total }) {
 	const pagesCount = Math.ceil(total / limit);
-	const pagesCut = getPagesCut({ pagesCount, pagesCutCount: 5, currentPage });
+	const pagesCut = getPagesCut({ pagesCount, pagesCutCount: 4, currentPage });
 	const pages = range(pagesCut.start, pagesCut.end);
 	const isFirstPage = currentPage === 1;
 	const isLastPage = currentPage === pagesCount;
@@ -19,7 +19,7 @@ function Pagination({ limit, onPageChange, currentPage, total }) {
 	}, [currentPage]);
 
 	function range(start, end) {
-		return [...Array(end - start).keys()].map((element) => element + start);
+		return [...Array(end - start + 1).keys()].map((element) => element + start);
 	}
 
 	function getPagesCut({ pagesCount, pagesCutCount, currentPage }) {
@@ -27,13 +27,13 @@ function Pagination({ limit, onPageChange, currentPage, total }) {
 		const floor = Math.floor(pagesCutCount / 2);
 
 		if (pagesCount < pagesCutCount) {
-			return { start: 1, end: pagesCount + 1 };
-		} else if (currentPage >= 1 && currentPage <= ceiling) {
-			return { start: 1, end: pagesCutCount + 1 };
+			return { start: 1, end: pagesCount };
+		} else if (currentPage <= ceiling) {
+			return { start: 1, end: pagesCutCount };
 		} else if (currentPage + floor >= pagesCount) {
-			return { start: pagesCount - pagesCutCount + 1, end: pagesCount + 1 };
+			return { start: pagesCount - pagesCutCount + 1, end: pagesCount };
 		} else {
-			return { start: currentPage - ceiling + 1, end: currentPage + floor + 1 };
+			return { start: currentPage - ceiling, end: currentPage + floor };
 		}
 	}
 
@@ -54,33 +54,46 @@ function Pagination({ limit, onPageChange, currentPage, total }) {
 	};
 
 	return (
-		
-		<>
-			<div className="all">
-				<>
-					<button className={currentPage===1 ? "activeStepPage" :"stepPage"} disabled={isFirstPage} onClick={handleFirstPage}>
-					<FiChevronsLeft/>
-					</button>
-					<button className={currentPage===1 ? "activeStepPage" :"stepPage"} disabled={isFirstPage} onClick={handlePrevPage}>
-						<FiChevronLeft/>
-					</button>
-					{pages.map((p) => (
-						<button
-							className={p === currentPage ? "current" : "notCurrent"}
-							onClick={() => onPageChange(p)}
-						>
-							{p}
-						</button>
-					))}
-					<button className={isLastPage ? "activeStepPage" :"stepPage"} disabled={isLastPage} onClick={handleNextPage}>
-						<FiChevronRight/>
-					</button>
-					<button className={isLastPage ? "activeStepPage" :"stepPage"} disabled={isLastPage} onClick={handleLastPage}>
-						<FiChevronsRight/>
-					</button>
-				</>
-			</div>
-		</>
+		<div className="all">
+			<button
+				className={currentPage === 1 ? "activeStepPage" : "stepPage"}
+				disabled={isFirstPage}
+				onClick={handleFirstPage}
+			>
+				<FiChevronsLeft />
+			</button>
+			<button
+				className={currentPage === 1 ? "activeStepPage" : "stepPage"}
+				disabled={isFirstPage}
+				onClick={handlePrevPage}
+			>
+				<FiChevronLeft />
+			</button>
+			{pages.map((p) => (
+				<button
+					key={p}
+					className={p === currentPage ? "current" : "notCurrent"}
+					onClick={() => onPageChange(p)}
+				>
+					{p}
+				</button>
+			))}
+			<button
+				className={isLastPage ? "activeStepPage" : "stepPage"}
+				disabled={isLastPage}
+				onClick={handleNextPage}
+			>
+				<FiChevronRight />
+			</button>
+			<button
+				className={isLastPage ? "activeStepPage" : "stepPage"}
+				disabled={isLastPage}
+				onClick={handleLastPage}
+			>
+				<FiChevronsRight />
+			</button>
+		</div>
 	);
 }
+
 export default Pagination;
