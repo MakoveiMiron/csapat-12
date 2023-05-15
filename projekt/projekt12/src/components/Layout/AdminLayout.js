f6c13d30023d62926d80a5012fc9c7a69e252b87
Pinke2k <pinkemihaly@gmail.com>
2023-05-15 19:34:01 +0200

    add admin login

--- projekt/projekt12/src/components/Layout/AdminLayout.js
+++ projekt/projekt12/src/components/Layout/AdminLayout.js
@@ -1,20 +1,11 @@
 import { Outlet } from "react-router-dom";
 import AdminNavigation from "../Nav/AdminNavigation";
-import { useContext } from "react";
-import { LoggedInUserContext } from "../../contexts/LoggedInUserContext";
-export default function AdminLayout(){
 
-    const [user,setUser] = useContext(LoggedInUserContext);
-
-    return(
-        <>
-        {user.uid === 'KqiGKLztHcVTxJukIfzbmUMgD8E3' ? <>
-                <AdminNavigation />
-                <Outlet/>
-                </> 
-                : 
-                "Ez csak az admin számára elérhető!"
-        }
-        </>
-    )
+export default function AdminLayout() {
+	return (
+		<>
+			<AdminNavigation />
+			<Outlet />
+		</>
+	);
 }
--- projekt/projekt12/src/components/Nav/AdminNavigation.js
+++ projekt/projekt12/src/components/Nav/AdminNavigation.js
@@ -1,13 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { AdminContext } from "../../contexts/AdminContext";
+import { auth } from "../../Constans/firebaseConfig";
+import { useContext } from "react";
+import { toast } from "react-toastify";
+import { signOut } from "firebase/auth";
 
-
-export default function AdminNavigation(){
-    return(
-        <>
-            <NavLink to="/admin/">Admin</NavLink>
-            <NavLink to="/admin/termek-felvetel">Termék felvétel</NavLink>
-            <NavLink to="/admin/termekek">Termékek</NavLink>
-            <NavLink to="/">Főoldal</NavLink>
-        </>
-    )
+export default function AdminNavigation() {
+	const [isAdmin, setIsAdmin] = useContext(AdminContext);
+	function handleLogOut() {
+		signOut(auth).then((resp) => {
+			setIsAdmin(false);
+			toast.success("Sikeres kijelentkezés!", {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		});
+	}
+	return (
+		<>
+			<NavLink to="/admin/">Admin</NavLink>
+			<NavLink to="/admin/termek-felvetel">Termék felvétel</NavLink>
+			<NavLink to="/admin/termekek">Termékek</NavLink>
+			<NavLink to="/">Főoldal</NavLink>
+			{isAdmin && <button onClick={handleLogOut}>Kijelentkeztes</button>}
+		</>
+	);
 }
