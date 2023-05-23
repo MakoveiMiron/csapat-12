6e97da4188b783213e8d4ca7e4b1af7c0cfeee83
MakoveiMiron <miron.makovei@gmail.com>
2023-05-23 15:02:22 +0200

    admin orders page done test needed

--- projekt/projekt12/src/pages/admin/AdminOrders.js
+++ projekt/projekt12/src/pages/admin/AdminOrders.js
@@ -1,57 +1,77 @@
 import { useContext, useState, useEffect } from "react"
 import { getOrderIds, readProducts, } from "../../services/Crud";
 import { readUsers } from "../../services/authCrud";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function AdminOrders(){
-    const [orders,setOrders] = useState({});
-    const [productList,setProductList] = useState([]);
+    const [orders, setOrders] = useState([]);
+    const [productList, setProductList] = useState([]);
+    const [users,setUsers] = useState([]);
+    const navigate = useNavigate()
 
     useEffect(() => {
         getOrderIds()
         .then(resp =>{
             setOrders(resp)
         });
-        readProducts()
-        .then(resp => setProductList(Object.values(resp)))
     },[]);
-    function findProduct(product){
-       const found = productList.find(id => id.id === product)
-       return found
-    }
 
-    async function findUser(uid){
-        let name;
-        readUsers("vasarlok",uid)
-        .then(resp => name = resp.name);
-        return name
-    }
-   
-    return(
-        <>
-            <div>
-                <h1>Admin rendelesek</h1>
-            </div>
-            
-                {Object.entries(orders).map((order,index)=> (
-                    <>
-                        <h2 key={order[0]}>{order[0]}</h2>
-                        <ul>
-                            <li>Vevő neve: {findUser(order[1].uid)}</li>
-                            <li key={order[1].id}>Rendelés id: {order[1].id}</li>
-                            <li key={order[1].uid}>Rendelő uid: {order[1].uid}</li>
-                            <p>Rendelt termékek:</p>
-                            
-                                {Object.entries(order[1].products).map( (product) => (
-                                        <ul>
-                                            <li>{findProduct(product[0]).title} {findProduct(product[0]).price}Ft</li>
-                                        </ul>
-                                ))}
-                            
-                        </ul>
-                    </>
-                ))}
-            
-        </>
-    )
+    useEffect(() => {
+        readProducts()
+        .then(resp => {
+            setProductList(resp);
+        })
+    },[]);
+
+    useEffect(() => {
+        readUsers("vasarlok")
+        .then(resp => setUsers(Object.values(resp)))
+    },[])
+       
+    function listProducts(orderId){
+        const order = Object.values(orders)
+        let result;
+        
+        order.forEach(ord => {
+            if(ord.id === orderId){
+                result = Object.entries(ord.products)
+            }
+        })
+        return result
+        }
+
+        return(
+            <>
+                <div>
+                    <h1>Admin rendelesek</h1>
+                    <div>
+                    {Object.keys(orders).map((orderId,index) => (
+                       <>   
+                            <h2>Rendelés ID: {orderId}</h2>
+                         <div>
+                            Rendelő adatai:
+                                <ul>
+                                    <li>Vevő neve: {Object.values(orders).map((order,idx) => users.map((user) => {if(index === idx && order.uid === user.uid){
+                                        return user.name
+                                        }}))}</li>
+                                    <li>Vevő uid: {Object.values(orders).map((order,idx) => users.map((user) => {if(index === idx && order.uid === user.uid){
+                                        return user.uid
+                                        }}))}
+                                    </li>
+                                </ul>
+
+                                Rendelés tartalma:
+                                <ul>
+                                    {listProducts(orderId).map(id => <li key={id}>{`${id[1]}db `}{id}{" (termek id)"}</li>)}
+                                </ul>
+                
+                               <button onClick={() => navigate(`/admin/megrendelesek/${orderId}`)}>Adatlap</button>
+                            </div>
+                        </>
+                    ))}
+                    </div>
+                </div>
+            </>
+        )
+    
 }
